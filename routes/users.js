32d3edac38b0d4f4a9274e9882a77704907f5e1f
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-12 15:22:11 +0700

    Restful Api users register

--- routes/users.js
+++ routes/users.js
@@ -17,4 +17,33 @@ router.get('/', function (req, res, next) {
     })
 });
 
+router.post('/register', function (req, res, next) {
+  console
+  if (!req.body.email) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Users
+  const options = {
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username
+  };
+
+  // Save Users in the database
+  Users.create(options)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Users."
+      });
+    });
+});
+
 module.exports = router;
