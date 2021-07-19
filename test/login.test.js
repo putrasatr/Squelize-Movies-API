a0e46370da9491c449c0835ade56ff43295d903b
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-19 13:48:01 +0700

    Tdd  login

--- test/login.test.js
+++ test/login.test.js
@@ -3,25 +3,22 @@ const request = require("supertest")
 const app = require("../app.js")
 
 const { expect } = chai
-const post = {
-  id: 1,
-  userId: 1,
-  images: 'cool.jpg',
-  likes: 1,
-  captions: null,
-  createdAt: '2021-07-14T03:20:53.070Z',
-  updatedAt: '2021-07-14T03:20:53.070Z',
-  user: { username: 'Admin', email: 'admin@gmail.com' }
+const body_send = {
+  email: 'admin@gmail.com',
+  password: '12345',
 }
 
 const codeRespose = 200
 const paginationTest = { page: 1, limit: 5, offset: 0, totalAll: 6, pages: 2 }
 describe('Fetch post test', async () => {
   it('Shows all stock states', async () => {
-    const { body, status } = await request(app).post('/users/login',);
-    const { data, pagination, code } = body;
-    expect(status).to.equal(200);
-    expect(data[0]).to.deep.include(post);
-    expect(pagination).to.deep.include(paginationTest);
+    const { body, status } = await request(app)
+      .post('/users/login',)
+      .send(body_send)
+      .set('Accept', 'application/json')
+    // const { data, pagination, code } = body;
+    // expect(status).to.equal(200);
+    // expect(data[0]).to.deep.include(post);
+    // expect(pagination).to.deep.include(paginationTest);
   });
 });
